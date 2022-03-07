220bac07664c0e9ec4d347e015590adf12e6b5f8
Omar Choudhry <sc20osc@leeds.ac.uk>
2022-03-07 20:11:15 +0000

    ui-staff: created landing page
    Co-Authored-By: Joshua Reiner <sc20jdr@leeds.ac.uk> 
    Co-Authored-By: Mohammed Fakeih <sc20mf@leeds.ac.uk> 
    Co-Authored-By: William Toon <sc20wt@leeds.ac.uk>

--- ui-staff/src/App.js
+++ ui-staff/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import {Dropdown, DropdownButton, InputGroup, FormControl, Button} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import "bootstrap/dist/css/bootstrap.css"
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+
 import './App.css';
 
-function App() {
+const center = [53.8, -1.55]
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div id="wrapper">
+          <MapContainer center={center} zoom={15}>
+              <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+              <Marker position={center}>
+                <Popup>You are here></Popup>
+              </Marker>
+          </MapContainer>
+          <div id="map-overlay">
+              <div id="top-bar">
+                  <InputGroup id="search-bar">
+                      <FormControl
+                          placeholder="Search"
+                          aria-label="Search"
+                          aria-describedby="basic-addon2"
+                      />
+                      <Button variant="secondary" id="button-addon2">
+                          <i><FontAwesomeIcon icon={faMagnifyingGlass}/></i>
+                      </Button>
+                  </InputGroup>
+                  <DropdownButton
+                      id="dropdown-basic-button"
+                      title={ <span><i><FontAwesomeIcon icon={faUser}/></i></span>}
+                      variant="secondary" menuVariant="dark">
+                        <Dropdown.Item href="#/action-1"><p>Log In</p></Dropdown.Item>
+                        <Dropdown.Item href="#/action-2"><p>Sign Up</p></Dropdown.Item>
+                        <Dropdown.Item href="#/action-3"><p>Toggle Dark mode</p></Dropdown.Item>
+                  </DropdownButton>
+              </div>
+          </div>
+      </div>
+  )
+};
+
 
 export default App;

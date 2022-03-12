e0bbfd14396f1d078f8c92f20fa3186cd2646965
Monty Zhuang <montyzhuang@gmail.com>
2022-03-12 20:18:38 +0000

    ui-customer: added vertical border line

--- ui-customer/src/AccountDetailsPage.js
+++ ui-customer/src/AccountDetailsPage.js
@@ -7,11 +7,12 @@ import {text} from "@fortawesome/fontawesome-svg-core";
 function CustomerDashboard(props) {
     const userDetails = [["Full Name", "Hashir Choudry"], ["UserID", 12345],  ["Email Address", "hashirsing@gmail.com"],
         ["Phone Number", "0774891234"], ["Password", "**********"]]
+
     return  (
         <>
             <Row >
                 {/*FIRST ROW*/}
-                <Col xs={4} className={"d-flex justify-content-end "}>
+                <Col xs={4} className={"d-flex justify-content-end border-end border-dark"}>
                     {/*FIRST COLUMN*/}
                     <h1>INERTIA</h1>
                 </Col>
@@ -21,12 +22,12 @@ function CustomerDashboard(props) {
                 </Col>
                 {/*SECOND COLUMN*/}
             </Row>
-            <Row>
+            <Row className={""}>
                 {/*SECOND ROW*/}
-                <Col xs={4}>
+                <Col xs={4} className={"border-end border-dark"}>
                     {/*FIRST COLUMN*/}
                     <Nav defaultActiveKey="/home" className="d-flex align-items-end flex-column">
-                        <Nav.Link href="/home">Account Information</Nav.Link>
+                        <Nav.Link href="/home">Account Info</Nav.Link>
                         <Nav.Link eventKey="link-1">Orders</Nav.Link>
                         <Nav.Link eventKey="link-2">Payment Details</Nav.Link>
                         <Nav.Link eventKey="disabled" disabled>
@@ -44,6 +45,7 @@ function CustomerDashboard(props) {
                             <tr>
                                 <td>{title[0]}</td>
                                 <td>{title[1]}</td>
+                                <td><u>edit</u></td>
                             </tr>
                         ))}
                         </tbody>

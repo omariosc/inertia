f8c5afccf7cc0d63bf1502a068bc35f925008648
Omar Choudhry <sc20osc@leeds.ac.uk>
2022-03-15 21:54:36 +0000

    ui-staff: Completed settings page. Added package for displaying images.
    Co-Authored-By: Josh Reiner <sc20jdr@leeds.ac.uk>
    
    Co-Authored-By: Mohammed Fakeih <sc20mf@leeds.ac.uk>

--- ui-staff/src/ManagerAccountManagement.js
+++ ui-staff/src/ManagerAccountManagement.js
@@ -8,15 +8,15 @@ function AccountManagement() {
             <h1>Account Management</h1>
             <h3>Name</h3>
             <InputGroup className="mb-3">
-                <input name="name" placeholder="Enter employee name" required/>
+                <input name="name" type="name" placeholder="Enter employee name" required/>
             </InputGroup>
             <h3>Username</h3>
             <InputGroup className="mb-3">
-                <input name="username" placeholder="Enter employee username" required/>
+                <input name="username" type="username" placeholder="Enter employee username" required/>
             </InputGroup>
             <h3>Email</h3>
             <InputGroup className="mb-3">
-                <input name="email" placeholder="Enter employee email" required/>
+                <input name="email" type="email" placeholder="Enter employee email" required/>
             </InputGroup>
             <Button bg="dark">Create account</Button>
         </>

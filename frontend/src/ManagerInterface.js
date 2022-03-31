53f09a345988065840c5ed5af47a94e67fda67f4
Hashir Ali <ed19h6a@leeds.ac.uk>
2022-03-31 03:09:20 +0100

    added dropdown for mobile responsive ui

--- frontend/src/ManagerInterface.js
+++ frontend/src/ManagerInterface.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Nav,  Row, Col} from "react-bootstrap";
+import {Nav, Row, Col, NavDropdown} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from './StaffDashboard.js';
 import ScooterManagement from './ManagerScooterManagement.js';
@@ -7,12 +7,10 @@ import Issues from './ManagerIssues.js';
 import Statistics from './ManagerStatistics.js';
 import AccountManagement from './ManagerAccountManagement.js';
 import StaffSettings from './StaffSettings.js';
-import './StaffInterface.css';
-
-import {NavMenu} from "./navbar";
+import useMediaQuery from "./useMediaQuery";
 
 export default function ManagerInterface({isDark, toggle, map_locations}) {
-    const [expanded, setExpanded] = useState(false);
+    const isMobile = useMediaQuery('(min-width: 768px)', {noSsr: true});
     const [showDashboard, setShowDashboard] = useState(true);
     const [showScooter, setShowScooter] = useState(false);
     const [showIssues, setShowIssues] = useState(false);
@@ -20,91 +18,169 @@ export default function ManagerInterface({isDark, toggle, map_locations}) {
     const [showAccounts, setShowAccounts] = useState(false);
     const [showSettings, setShowSettings] = useState(false);
 
-    return (
-        <div className="clickable">
-            <Row>
-                <Col xs={2} className="border-end border-dark">
-                    <Nav
-                        defaultActiveKey="#/dashboard"
-                        variant="pills"
-                        className="flex-column"
-                        style={{paddingLeft: "15px", color: "black"}}
-                    >
-                        <NavMenu
+
+        return (
+            <div className="clickable">
+                {isMobile ?
+                <Row>
+                    <Col xs={2} className="border-end border-dark">
+                        <Nav
+                            defaultActiveKey="#/dashboard"
+                            variant="pills"
                             className="flex-column"
                             style={{paddingLeft: "15px", color: "black"}}
                         >
+                                <Nav.Link onClick={() => {
+                                    setShowDashboard(true);
+                                    setShowScooter(false);
+                                    setShowIssues(false);
+                                    setShowStatistics(false);
+                                    setShowAccounts(false);
+                                    setShowSettings(false);
+                                }}
+                                          href="#/dashboard">Dashboard</Nav.Link>
+                                <Nav.Link onClick={() => {
+                                    setShowDashboard(false);
+                                    setShowScooter(true);
+                                    setShowIssues(false);
+                                    setShowStatistics(false);
+                                    setShowAccounts(false);
+                                    setShowSettings(false);
+                                }}
+                                          href="#/scooters">Scooter Management</Nav.Link>
+                                <Nav.Link onClick={() => {
+                                    setShowDashboard(false);
+                                    setShowScooter(false);
+                                    setShowIssues(true);
+                                    setShowStatistics(false);
+                                    setShowAccounts(false);
+                                    setShowSettings(false);
+                                }}
+                                          href="#/issues">Issues</Nav.Link>
+                                <Nav.Link onClick={() => {
+                                    setShowDashboard(false);
+                                    setShowScooter(false);
+                                    setShowIssues(false);
+                                    setShowStatistics(true);
+                                    setShowAccounts(false);
+                                    setShowSettings(false);
+                                }}
+                                          href="#/statistics">Statistics</Nav.Link>
+                                <Nav.Link onClick={() => {
+                                    setShowDashboard(false);
+                                    setShowScooter(false);
+                                    setShowIssues(false);
+                                    setShowStatistics(false);
+                                    setShowAccounts(true);
+                                    setShowSettings(false);
+                                }}
+                                          href="#/accounts">Account Management</Nav.Link>
+                                <Nav.Link onClick={() => {
+                                    setShowDashboard(false);
+                                    setShowScooter(false);
+                                    setShowIssues(false);
+                                    setShowStatistics(false);
+                                    setShowAccounts(false);
+                                    setShowSettings(true);
+                                }}
+                                          href="#/settings">Settings</Nav.Link>
+                        </Nav>
+                    </Col>
+                    <Col xs={9}>
+                        <>
+                            {showDashboard ? <Dashboard onHide={() => setShowDashboard(false)}/> : null}
+                            {showScooter ? <ScooterManagement map_locations={map_locations}
+                                                              onHide={() => setShowScooter(false)}/> : null}
+                            {showIssues ? <Issues onHide={() => setShowIssues(false)}/> : null}
+                            {showStatistics ?
+                                <Statistics isDark={isDark} onHide={() => setShowStatistics(false)}/> : null}
+                            {showAccounts ? <AccountManagement onHide={() => setShowAccounts(false)}/> : null}
+                            {showSettings ? <StaffSettings isDark={isDark} toggle={toggle}
+                                                           onHide={() => setShowSettings(false)}/> : null}
+                        </>
+                    </Col>
+                </Row>
+                    :
+                    <div>
+                        <Nav
+                            defaultActiveKey="#/dashboard"
+                            variant="pills"
+                            className="flex-row"
+                            style={{paddingLeft: "15px", color: "black"}}
+                        >
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item onClick={() => {
+                                    setShowDashboard(true);
+                                    setShowScooter(false);
+                                    setShowIssues(false);
+                                    setShowStatistics(false);
+                                    setShowAccounts(false);
+                                    setShowSettings(false);
+                                }}
+                                          href="#/dashboard">Dashboard</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {
+                                setShowDashboard(false);
+                                setShowScooter(true);
+                                setShowIssues(false);
+                                setShowStatistics(false);
+                                setShowAccounts(false);
+                                setShowSettings(false);
+                            }}
+                                      href="#/scooters">Scooter Management</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {
+                                setShowDashboard(false);
+                                setShowScooter(false);
+                                setShowIssues(true);
+                                setShowStatistics(false);
+                                setShowAccounts(false);
+                                setShowSettings(false);
+                            }}
+                                      href="#/issues">Issues</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {
+                                setShowDashboard(false);
+                                setShowScooter(false);
+                                setShowIssues(false);
+                                setShowStatistics(true);
+                                setShowAccounts(false);
+                                setShowSettings(false);
+                            }}
+                                      href="#/statistics">Statistics</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {
+                                setShowDashboard(false);
+                                setShowScooter(false);
+                                setShowIssues(false);
+                                setShowStatistics(false);
+                                setShowAccounts(true);
+                                setShowSettings(false);
+                            }}
+                                      href="#/accounts">Account Management</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {
+                                setShowDashboard(false);
+                                setShowScooter(false);
+                                setShowIssues(false);
+                                setShowStatistics(false);
+                                setShowAccounts(false);
+                                setShowSettings(true);
+                            }}
+                                      href="#/settings">Settings</NavDropdown.Item>
+                            </NavDropdown>
 
-                        <Nav.Link onClick={() => {
-                            setShowDashboard(true);
-                            setShowScooter(false);
-                            setShowIssues(false);
-                            setShowStatistics(false);
-                            setShowAccounts(false);
-                            setShowSettings(false);
-                        }}
-                                  href="#/dashboard">Dashboard</Nav.Link>
-                        <Nav.Link onClick={() => {
-                            setShowDashboard(false);
-                            setShowScooter(true);
-                            setShowIssues(false);
-                            setShowStatistics(false);
-                            setShowAccounts(false);
-                            setShowSettings(false);
-                        }}
-                                  href="#/scooters">Scooter Management</Nav.Link>
-                        <Nav.Link onClick={() => {
-                            setShowDashboard(false);
-                            setShowScooter(false);
-                            setShowIssues(true);
-                            setShowStatistics(false);
-                            setShowAccounts(false);
-                            setShowSettings(false);
-                        }}
-                                  href="#/issues">Issues</Nav.Link>
-                        <Nav.Link onClick={() => {
-                            setShowDashboard(false);
-                            setShowScooter(false);
-                            setShowIssues(false);
-                            setShowStatistics(true);
-                            setShowAccounts(false);
-                            setShowSettings(false);
-                        }}
-                                  href="#/statistics">Statistics</Nav.Link>
-                        <Nav.Link onClick={() => {
-                            setShowDashboard(false);
-                            setShowScooter(false);
-                            setShowIssues(false);
-                            setShowStatistics(false);
-                            setShowAccounts(true);
-                            setShowSettings(false);
-                        }}
-                                  href="#/accounts">Account Management</Nav.Link>
-                        <Nav.Link onClick={() => {
-                            setShowDashboard(false);
-                            setShowScooter(false);
-                            setShowIssues(false);
-                            setShowStatistics(false);
-                            setShowAccounts(false);
-                            setShowSettings(true);
-                        }}
-                                  href="#/settings">Settings</Nav.Link>
 
-                        </NavMenu>
-                    </Nav>
-                </Col>
-                <Col xs={9}>
-                    <>
+                        </Nav>
+
                         {showDashboard ? <Dashboard onHide={() => setShowDashboard(false)}/> : null}
-                        {showScooter ? <ScooterManagement map_locations={map_locations} onHide={() => setShowScooter(false)}/> : null}
+                        {showScooter ? <ScooterManagement map_locations={map_locations}
+                                                          onHide={() => setShowScooter(false)}/> : null}
                         {showIssues ? <Issues onHide={() => setShowIssues(false)}/> : null}
-                        {showStatistics ? <Statistics isDark={isDark} onHide={() => setShowStatistics(false)}/> : null}
+                        {showStatistics ?
+                            <Statistics isDark={isDark} onHide={() => setShowStatistics(false)}/> : null}
                         {showAccounts ? <AccountManagement onHide={() => setShowAccounts(false)}/> : null}
                         {showSettings ? <StaffSettings isDark={isDark} toggle={toggle}
                                                        onHide={() => setShowSettings(false)}/> : null}
-                    </>
-                </Col>
-            </Row>
-        </div>
-    );
+
+                        </div>}
+            </div>
+
+        );
+
 };
